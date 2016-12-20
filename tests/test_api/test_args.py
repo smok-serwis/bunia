5d27d329513a35b745e35e85875d43910dfe968d
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2016-12-20 01:47:08 +0100

    +travis CI + tests

---
+++ tests/test_api/test_args.py
@@ -0,0 +1,24 @@
+import unittest
+
+from bunia.api import Argument, Integer
+
+class TestArguments(unittest.TestCase):
+
+    def test_base(self):
+        arg = Argument('test', description=u'desc', required=False, default='wtf')
+
+        self.assertEquals(arg.name, 'test')
+        self.assertEquals(arg.description, u'desc')
+        self.assertEquals(arg.required, False)
+        self.assertEquals(arg.default, 'wtf')
+
+    def test_argument(self):
+        arg = Argument('test')
+
+        self.assertEquals(arg.name, 'test')
+        self.assertEquals('k', arg.clean('k'))
+
+    def test_integer(self):
+        arg = Integer('test')
+        self.assertEquals(2, arg.clean('2'))
+        self.assertRaises(ValueError, lambda: arg.clean('weer'))
