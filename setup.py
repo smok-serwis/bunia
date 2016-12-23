05dae566a81eb11cece01a85b45c5c931c8346cd
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2016-12-23 20:54:11 +0100

    moar pythons

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(name='bunia',
-      version='0.6',
+      version='0.7-devel',
       description='Write commands that use multiple interfaes',
       author='DMS Serwis s.c.',
       author_email='piotrm@smok.co',
       url='https://github.com/smok-serwis/bunia',
-      download_url='https://github.com/smok-serwis/bunia/archive/master.zip',
+      download_url='https://github.com/smok-serwis/bunia/archive/v0.6.zip',
       keywords=['arguments', 'argparse', 'html', 'console'],
       platforms=['any'],
       packages=['bunia',
@@ -19,6 +19,10 @@ setup(name='bunia',
       requires=[
           'six'
       ],
+      tests_require=[
+          "nose"
+      ],
+      test_suite='nose.collector',
       license='MIT License',
       long_description='''This is an interface for writing commands that can use multiple interfaces, ie. you can launch
 it from shell, Web, SMS, etc.
@@ -27,7 +31,10 @@ A device-independent output and input is presented.''',
       classifiers=[
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: Implementation :: CPython',
           'Programming Language :: Python :: Implementation :: PyPy',
           'Operating System :: OS Independent'
