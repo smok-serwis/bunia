3ee6678b977987530ff654def183b57b1a4a592b
Krzysztof Bąk <contact@itachi.pl>
2018-03-30 16:58:03 +0200

    Fixed missing init entries.

--- bunia/output/__init__.py
+++ bunia/output/__init__.py
@@ -4,3 +4,4 @@ Outputs and their implementations
 
 from bunia.output.base import Output
 from bunia.output.cons import ConsoleOutput
+from bunia.output.bytes import BytesOutput
--- bunia/runner/__init__.py
+++ bunia/runner/__init__.py
@@ -2,4 +2,4 @@ from bunia.runner.base import Runner
 from bunia.runner.console import ConsoleRunner
 from bunia.runner.html import HTMLOutputBuilder, HTMLRunner
 from bunia.runner.inline import InlineRunner
-
+from bunia.runner.file import FileRunner
